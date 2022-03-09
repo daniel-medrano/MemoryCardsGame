e30fa49f77317cfade3c902281579606b139ba34
jdmedrano16 <69618332+jdmedrano16@users.noreply.github.com>
2022-03-09 15:47:21 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+const cards = document.querySelectorAll(".card");
+const restartBttn = document.getElementById("restart");
+const attempts = document.getElementById("attempts");
+const failedAttempts = document.getElementById("failed-attempts");
+
+let gamePaused = false;
+let firstCard = null;
+let secondCard = null;
+
+let cardsDisabled = [];
+let numAttempts = 0;
+let numFailedAttempts = 0;
+
+function isFirstCardFlipped() {
+    return firstCard != null;
+}
+
+function areTheSame(firstCard, secondCard) {
+    return firstCard.id == secondCard.id;
+}
+
+function flipCard() {
+    if (gamePaused)
+        return;
+    // Si la primera carta no ha sido volteada
+    if (!isFirstCardFlipped()) {
+        firstCard = this;
+        this.classList.add("flipped");
+        return;
+    } 
+    if (!areTheSame(firstCard, this)) {
+        secondCard = this;
+        this.classList.add("flipped");
+        compareCards();
+    }
+}
+
+function disableCards() {
+    cardsDisabled.push(firstCard, secondCard);
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetPair();
+}
+
+function unflipCards() {
+    gamePaused = true;
+    // Toma 1.1 segundos el volver a voltear las cartas.
+    setTimeout(function() {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetPair();
+    }, 1100);
+}
+
+function isGameWon() {
+    setTimeout(() => {
+        let isGameWon = cardsDisabled.length == cards.length;
+        if (isGameWon) {
+            alert("You have won the game!")
+        }
+    }, 550);
+}
+
+function increaseAttempts() {
+    numAttempts++;
+    attempts.innerText = numAttempts;
+}
+
+function increaseFailedAttempts() {
+    numFailedAttempts++;
+    failedAttempts.innerText = numFailedAttempts;
+}
+
+function compareCards() {
+    let matched = firstCard.id.slice(-1) == secondCard.id.slice(-1);
+
+    if (matched) {
+        increaseAttempts();
+        disableCards();
+        isGameWon();
+    } else {
+        increaseAttempts();
+        increaseFailedAttempts();
+        unflipCards();
+    }
+}
+
+function resetPair() {
+    firstCard = null;
+    secondCard = null;
+    gamePaused = null;
+}
+
+function resetStats() {
+    numAttempts = 0;
+    numFailedAttempts = 0;
+    attempts.innerText = numAttempts;
+    failedAttempts.innerText = numFailedAttempts;
+}
+
+function disorderCards() {
+    cards.forEach(card => {
+        let position = Math.round(Math.random() * 12);
+        card.style.order = position;
+    });
+}
+
+function unflipAllCards() {
+    cards.forEach(card => {
+        let isFlipped = card.classList.contains("flipped");
+        if (isFlipped) {
+            card.classList.remove("flipped");
+        }
+    });
+}
+
+function enableCards() {
+    cardsDisabled.forEach(card => card.addEventListener("click", flipCard));
+    cardsDisabled = [];
+}
+
+function restartGame() {
+    unflipAllCards();
+    enableCards();
+    disorderCards();
+    resetPair();
+    resetStats();
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
+restartBttn.addEventListener("click", restartGame);
+
+disorderCards();
